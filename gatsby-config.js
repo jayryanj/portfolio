1041db39c9453cd097f1c6265353c70b2a4f965e
Jay Ryan Jamorabon <43054912+jayryanj@users.noreply.github.com>
2022-01-09 23:32:24 -1000

    Added Material-UI

--- gatsby-config.js
+++ gatsby-config.js
@@ -46,6 +46,7 @@ module.exports = {
     "gatsby-plugin-react-helmet",
     "gatsby-plugin-sharp",
     "gatsby-plugin-image",
+    "gatsby-plugin-material-ui",
     {
       resolve: "gatsby-source-contentful",
       options: contentfulConfig,
--- src/components/hero.js
+++ src/components/hero.js
@@ -1,14 +1,17 @@
 import React from 'react'
 import { GatsbyImage } from 'gatsby-plugin-image'
+import Container from '@mui/material/Container';
 
 import * as styles from './hero.module.css'
 
 const Hero = () => {
     return(
         <div className={styles.hero}>
+            <Container>
             <p>
                 Aloha, my name is Jay
             </p>
+            </Container>
         </div>
     );
 }
