ab19f96e45d96b2af6d2b0a936d819854ee62acb
Jay Ryan Jamorabon <43054912+jayryanj@users.noreply.github.com>
2022-01-16 22:15:17 -1000

    Added grid to Footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,14 +1,31 @@
 import React from 'react'
 
 import Container from './container'
+import { Grid } from "@mui/material";
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import * as styles from '../styles/footer.module.css'
 
 const Footer = () => (
     <div className={styles.footer}>
         <Container as="footer">
-            <p className={styles.text}>
-                Copyright &#169;{new Date().getFullYear()} Jay Ryan Jamorabon
-            </p>
+            <Grid container>
+                <Grid item md={6} sm={12}>
+                    <p className={styles.text}>
+                        Copyright &#169;{new Date().getFullYear()} Jay Ryan Jamorabon
+                    </p>
+                </Grid>
+                <Grid item md={6} sm={12}>
+                    <ul className={styles.social}>
+                        <li className={styles.socialItem}>
+                            <GitHubIcon />
+                        </li>
+                        <li className={styles.socialItem}>
+                            <LinkedInIcon />
+                        </li>
+                    </ul>
+                </Grid>
+            </Grid>
         </Container>
     </div>
 
