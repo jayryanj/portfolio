47b13d9d0cd447fda3c13f033df08453dd534012
Jay Ryan Jamorabon <43054912+jayryanj@users.noreply.github.com>
2022-01-18 10:28:39 -1000

    Updated Footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -6,42 +6,40 @@ import { StaticImage } from 'gatsby-plugin-image'
 import { Grid } from "@mui/material";
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import MailOutlinedIcon from '@mui/icons-material/MailOutlined';
+
 import * as styles from '../styles/footer.module.css'
 
 const Footer = () => (
     <div className={styles.footer}>
         <Container as="footer">
-            <Grid container>
-                <Grid className={styles.topRow} item sm={12} md={12}>
-                    <StaticImage className={styles.logo} src="../images/cartoon_portrait_new.png" alt="Jay"/>
-                    <p>Play and Learn</p>
-                </Grid>
-                <Grid item md={6} sm={12}>
-                    <p className={styles.text}>
-                        Created by Jay &#169; {new Date().getFullYear()}
-                    </p>
-                </Grid>
-                <Grid item md={6} sm={12}>
-                    <p>
-                        <ul className={styles.social}>
-                            <li>
-                                Profiles:
-                            </li>
-                            <li className={styles.socialItem}>
-                                <Link target="_blank" to="https://github.com/jayryanj">
-                                    <GitHubIcon />
-                                </Link>
-                            </li>
-                            <li className={styles.socialItem}>
-                                <Link target="_blank" to="https://www.linkedin.com/in/jayryanjamorabon/">
-                                    <LinkedInIcon />
-                                </Link>
-                                
-                            </li>
-                        </ul>
-                    </p>
-                </Grid>
-            </Grid>
+            <StaticImage className={styles.logo} src="../images/cartoon_portrait_new.png" alt="Jay"/>
+            <p className={styles.quote}>"Never stop learning <br/>because life never stops teaching"</p>
+            <p>
+                <ul className={styles.social}>
+                    <li className={styles.socialItem}>
+                        <Link target="_blank" to="https://github.com/jayryanj">
+                            <GitHubIcon className={styles.socialIcon} />
+                        </Link>
+                    </li>
+                    <li className={styles.socialItem}>
+                        <Link target="_blank" to="https://www.linkedin.com/in/jayryanjamorabon/">
+                            <LinkedInIcon className={styles.socialIcon} />
+                        </Link>
+                    </li>
+                    <li className={styles.socialItem}>
+                        <a target="_blank" href="mailto:jayrjamorabon@gmail.com">
+                            <MailOutlinedIcon className={styles.socialIcon} />
+                        </a>
+                    </li>
+                </ul>
+            </p>
+            <p className={styles.copyright}>
+                Created by Jay Ryan Jamorabon &#169; {new Date().getFullYear()}
+            </p>
+            <p className={styles.madeWith}>
+                Made with <span><StaticImage className={styles.gatsbyLogo} src="../images/Gatsby_Logo_White.png" alt="Gatsby" /></span>
+            </p>
         </Container>
     </div>
 
