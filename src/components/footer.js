e84a1851fb4fcf64be28872a1c5c7c4b5dd39595
Jay Ryan Jamorabon <43054912+jayryanj@users.noreply.github.com>
2022-01-17 15:47:11 -1000

    Added social links to Footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,6 +1,7 @@
 import React from 'react'
-
 import Container from './container'
+import { Link } from 'gatsby';
+
 import { Grid } from "@mui/material";
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
@@ -16,14 +17,24 @@ const Footer = () => (
                     </p>
                 </Grid>
                 <Grid item md={6} sm={12}>
-                    <ul className={styles.social}>
-                        <li className={styles.socialItem}>
-                            <GitHubIcon />
-                        </li>
-                        <li className={styles.socialItem}>
-                            <LinkedInIcon />
-                        </li>
-                    </ul>
+                    <p>
+                        <ul className={styles.social}>
+                            <li>
+                                Profiles:
+                            </li>
+                            <li className={styles.socialItem}>
+                                <Link target="_blank" to="https://github.com/jayryanj">
+                                    <GitHubIcon />
+                                </Link>
+                            </li>
+                            <li className={styles.socialItem}>
+                                <Link target="_blank" to="https://www.linkedin.com/in/jayryanjamorabon/">
+                                    <LinkedInIcon />
+                                </Link>
+                                
+                            </li>
+                        </ul>
+                    </p>
                 </Grid>
             </Grid>
         </Container>
