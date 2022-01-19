aa7119888fa1af585a8bc138a91f8f48207e859a
Jay Ryan Jamorabon <43054912+jayryanj@users.noreply.github.com>
2022-01-19 01:31:03 -1000

    Updated reveal animations for About

--- src/components/about.js
+++ src/components/about.js
@@ -47,11 +47,11 @@ const About = () => {
                             <h3 className={styles.name}>
                                 Jay Ryan Jamorabon
                             </h3>
-                            <div data-sal="slide-up" className={styles.description}>
-                                <p>
+                            <div className={styles.description}>
+                                <p data-sal="slide-up">
                                 <strong>Aloha! My name is Jay</strong>. I'm a <strong>software engineer</strong> and a recent college graduate from the islands of <strong>Hawai'i</strong>.
                                 </p>
-                                <p>
+                                <p data-sal="slide-up" data-sal-delay="300">
                                     My interest in coding started in the seventh grade with my very first "Hello, world!" in C. Now, my interest has spiraled into a passion for developing applications and solving problems with technology.
                                 </p>
                             </div>
